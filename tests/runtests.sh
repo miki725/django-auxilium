716aa66abafa12b0581fa29093ebcc9a7befd2c0
Miroslav Shubernetskiy <miroslav@miki725.com>
2013-02-27 15:55:13 -0500

    modified tests runner to support running tests in IDEs like PyCharm. since manage.py exists, the tests folder can be considered as a Django project hence IDEs can be configured to run django tests

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -1,14 +1,6 @@
 #!/bin/sh
 
-export PYTHONPATH="./"
-export DJANGO_SETTINGS_MODULE="settings"
-
-if [ `which django-admin.py` ] ; then
-    export DJANGO_ADMIN=django-admin.py
-else
-    export DJANGO_ADMIN=django-admin
-fi
-
+export DJANGO_MANAGE=manage.py
 export args="$@"
 
 # run only django_auxilium tests if not arguments are provided
@@ -16,4 +8,4 @@ if [ -z "$args" ] ; then
     export args="django_auxilium"
 fi
 
-$DJANGO_ADMIN test --settings=$DJANGO_SETTINGS_MODULE --pythonpath="../" "$args"
+python $DJANGO_MANAGE test "$args"
