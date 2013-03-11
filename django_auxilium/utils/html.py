84f2517da41223454e06fbfeee9515032c52a8ee
Miroslav Shubernetskiy <miroslav@miki725.com>
2013-03-11 19:49:59 -0400

    fixed bug in minify html

--- django_auxilium/utils/html.py
+++ django_auxilium/utils/html.py
@@ -11,7 +11,7 @@ RE_NEWLINE = re.compile(r"\n")
 def simple_minify(html):
     html = strip_spaces_between_tags(html.strip())
     html = RE_MULTISPACE.sub(' ', html)
-    html = RE_NEWLINE.sub('', html)
+    html = RE_NEWLINE.sub(' ', html)
     return html
 
 
