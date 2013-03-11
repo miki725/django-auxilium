a261ae2e6a69613901879a419035af683b5d0a63
Miroslav Shubernetskiy <miroslav@miki725.com>
2013-03-11 18:24:31 -0400

    added ability to pass regular expressions for multiplevaluesfield. also changed it's docs.

--- django_auxilium/forms/multiple_values.py
+++ django_auxilium/forms/multiple_values.py
@@ -1,36 +1,53 @@
+import re
 from django import forms
 from django.core.validators import EMPTY_VALUES
 from django.utils.translation import ugettext_lazy as _
 
 
+retype = type(re.compile(''))
+
+
 class MultipleValuesField(forms.CharField):
     """
-    Form field to allow users enter values as comma separated values. Inherits from
-    ``CharField`` so accepts al ``CharField`` parameters.
+    Form field to allow users to enter multiple values.
+
+    The best approach to enter multiple values is to provide a user with multiple
+    input fields however sometimes that is not feasible. In that case this field
+    provides a way for a user to enter multiple values in a single input field. The
+    values can be delimited by either a constant character(s) or even by a regular
+    expression.
 
     Parameters
     ----------
-    delimiter : str
-        The delimiter according to which the values are split. Default is ``,``.
+    delimiter : str, re, optional
+        The delimiter according to which the values are split. It can also be given as a
+        compiled regular expression (e.g. ``re.compile('\W+')``).
+        Default is ``,`` (comma).
     mapping : dict, callable, optional
-        By default all split values are casted to string. If mapping is provided,
-        it allows to define a mapping for specific strings to values they should return.
-        If callable is provided instead of a ``dict``, then to get the correct Python value,
-        it is called with the string value. Default is ``None``.
+        By default all split values are casted to a Python string. The mapping allows to
+        change that so that individual values will be mapped to different data-types.
+        Mapping can be defined as a callable which will should accept one parameter, the
+        value, and return the input value properly casted. Mapping can also be defined
+        as a dictionary. In that case, if the individual value exists as a key of the
+        dictionary, the value associated with the key will be used as a final casted
+        value. Please note that if the key is not found in the dictionary, then the input
+        value will be used.
+        Default is ``None``.
     max_values : int, optional
         The maximum allowed number of values. Default is ``None``.
     min_values : int, optional
         The minimum required number of provided values. Default is ``None``.
     strip : bool, optional
         If ``True``, then once the user string is split, all values are stripped
-        of whitespace on either side of the string before being converted to Python value.
+        of whitespace on either side of the string before being converted to Python value
+        by using Python's string ``strip()``.
         Default is ``True``.
     disregard_empty : bool, optional
-        If ``True``, then once user string is split, all false-equivalent values are disregarded.
-        Default is ``True``.
+        If ``True``, then once input string is split, all false evaluated values are
+        disregarded. Default is ``True``.
     invalid_values : list, optional
-        If provided, then all converted values are checked against this list and if any values
-        exist, then error is reported. Default is ``None``.
+        If provided, this list determines which values are invalid and if any are
+        encountered, a ``ValidationError`` will be raised.
     """
     default_error_messages = {
         'max_values': _(u'More values than allowed. Entered {} and allowed {}.'),
@@ -62,7 +79,10 @@ class MultipleValuesField(forms.CharField):
         if value in EMPTY_VALUES:
             return []
 
-        values = value.split(self.delimiter)
+        if isinstance(self.delimiter, retype):
+            values = self.delimiter.split(value)
+        else:
+            values = value.split(self.delimiter)
 
         if self.strip:
             values = [i.strip() for i in values]
@@ -70,23 +90,36 @@ class MultipleValuesField(forms.CharField):
         if self.disregard_empty:
             values = [i for i in values if i]
 
-        if self.min_values and len(values) < self.min_values:
-            raise forms.ValidationError(self.error_messages['max_values'].format(len(values), self.min_values))
-
-        if self.max_values and len(values) > self.max_values:
-            raise forms.ValidationError(self.error_messages['max_values'].format(len(values), self.max_values))
-
         if self.mapping:
             if not callable(self.mapping):
                 values = [self.mapping.get(i, i) for i in values]
             else:
                 values = [self.mapping(i) for i in values]
 
+        return values
+
+    def validate(self, values):
+        super(MultipleValuesField, self).validate(values)
+
+        if self.min_values and len(values) < self.min_values:
+            raise forms.ValidationError(
+                self.error_messages['max_values'].format(len(values), self.min_values)
+            )
+
+        if self.max_values and len(values) > self.max_values:
+            raise forms.ValidationError(
+                self.error_messages['max_values'].format(len(values), self.max_values)
+            )
+
         if self.invalid_values:
             invalid_values = set(values).intersection(self.invalid_values)
             if invalid_values:
                 raise forms.ValidationError(
-                    self.error_messages['invalid_value'].format(", ".join([str(i) for i in invalid_values]),
-                                                                'is' if len(invalid_values) == 1 else 'are'))
+                    self.error_messages['invalid_value'].format(
+                        ", ".join([str(i) for i in invalid_values]),
+                        'is' if len(invalid_values) == 1 else 'are')
+                )
 
-        return values
+    def run_validators(self, values):
+        for v in values:
+            super(MultipleValuesField, self).run_validators(v)
