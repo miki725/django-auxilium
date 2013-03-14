cf27b39b0b3e1f4d26a5c7a595d2de8ed4695815
Miroslav Shubernetskiy <miroslav@miki725.com>
2013-03-14 01:14:53 -0400

    added minify middleware

---
+++ django_auxilium/middleware/html.py
@@ -0,0 +1,19 @@
+import re
+from django.conf import settings
+from django.core.exceptions import MiddlewareNotUsed
+from django_auxilium.utils.html import simple_minify
+
+
+class MinifyHTMLMiddleware(object):
+    def __init__(self):
+        if settings.DEBUG:
+            raise MiddlewareNotUsed
+
+    def process_response(self, request, response):
+        header = 'Content-Type'
+        if response.has_header(header) and 'text/html' in response[header]:
+            try:
+                response.content = simple_minify(response.content.strip())
+            except:
+                pass
+        return response
