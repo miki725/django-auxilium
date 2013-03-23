296b0120787405dd18fd058b58361e12014e5415
Miroslav Shubernetskiy <miroslav@miki725.com>
2013-03-23 16:44:24 -0400

    updated run tests.sh to be able to specify addition test runner flags instead of apps

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -3,9 +3,4 @@
 export DJANGO_MANAGE=manage.py
 export args="$@"
 
-# run only django_auxilium tests if not arguments are provided
-if [ -z "$args" ] ; then
-    export args="django_auxilium"
-fi
-
-python $DJANGO_MANAGE test "$args"
+python $DJANGO_MANAGE test django_auxilium "$args"
